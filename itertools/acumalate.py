8102211f5e48d7c4a6c32c67e90af730e3878240
Olimjon <olimjonsn233@gmail.com>
2020-11-01 16:07:08 +0500

    place and multiple

--- itertools/acumalate.py
+++ itertools/acumalate.py
@@ -1,5 +1,9 @@
 from itertools import accumulate
+import operator
 a= [1,2,3,4]
 # add to the number before it
 acc=accumulate(a)
 print(list(acc))
+#multiple to the number before it
+amm=accumulate(a,func=operator.mul)
+print(list(amm))
